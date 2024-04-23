ef080aace9d31a93d0b660ecd761d5458303e15b
sdelgado0125 <sdelgado0125@gmail.com>
2024-04-23 00:11:04 -0400

    second

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from stories import story
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secretstory"
+
+@app.route("/")
+def ask_questions():
+
+    prompts = story.prompts
+
+    return render_template("questions.html", prompts=prompts)
+
+
+@app.route("/story")
+def show_story():
+
+    text = story.generate(request.args)
+
+    return render_template("stories.html", text=text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
